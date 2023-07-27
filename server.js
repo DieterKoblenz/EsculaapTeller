214acda9ba82c318794f97deebf62fb4a7969667
DieterKoblenz <mail@jeroenvanveen.nl>
2023-07-27 07:06:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-// Todo:
-// Publieke opdrachten: #punten !top5
-// Private opdrachten: !nieuw !setnickname !acties
-// 
-//
-
+const fs = require('fs');
 const { Client, LocalAuth, MessageMedia  } = require('whatsapp-web.js');
 const sqlite3 = require('sqlite3').verbose();
 const qrcode = require('qrcode-terminal');
 
 const eCIID = '120363162802059783@g.us'; // Replace with your own eCIID (Deze code is om te testen)
+
+console.error('Starting the Esculaap app.');
 
 // Create or open the SQLite database
 const db = new sqlite3.Database('scores.db', (err) => {
@@ -26,23 +23,27 @@ const db = new sqlite3.Database('scores.db', (err) => {
       )
     `);
     db.run(`
-    CREATE TABLE IF NOT EXISTS points_changes (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      user TEXT NOT NULL,
-      points_change INTEGER NOT NULL,
-      timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-    )
-  `);
+      CREATE TABLE IF NOT EXISTS points_log (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user TEXT NOT NULL,
+        score_change INTEGER NOT NULL,
+        chick TEXT,
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+      )
+    `);
   }
 });
 
 const client = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+    puppeteer: {headless: true, executablePath: '/usr/bin/google-chrome', args: ['--no-sandbox', '--disable-setuid-sandbox']}
+	
 });
  
-	
-client.initialize();	
+console.log('Initialize Client');
 
+client.initialize();	
+console.log('Get QR');
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
 });
@@ -61,15 +62,19 @@ client.on('ready', () => {
 });
 
 client.on('message', async (msg) => {
-  const groupChatId = msg.from.endsWith('@g.us') ? msg.from : null;
-  if (groupChatId === eCIID) { // Alleen antwoorden in eCI groupchat
+  const Userid = msg.author;
+  const ChatId = msg.from.endsWith('@g.us') ? msg.from : null;
+  console.log(`Message received on channel ${ChatId} and from ${Userid}`);
+  console.log(msg);
+  // Alleen antwoorden in eCI groupchat
+  if (ChatId === eCIID) {
     if (msg.body == '!top5') {
       const getScoresWithNicknames = await getTopScoresWithNicknames();
       if (getScoresWithNicknames.length > 0) {
         const scoreMessage = getScoresWithNicknames
           .map(({ nickname, score }) => `${nickname}: ${score}`)
           .join('\n');
-        msg.reply(`Scores:\n${scoreMessage}`);
+        msg.reply(`Onze top 5 🥇:\n${scoreMessage}`);
       } else {
         msg.reply('No scores found.');
       }
@@ -77,46 +82,76 @@ client.on('message', async (msg) => {
       const nickname = msg.body.split(' ')[1]; // Extract the nickname from the message
 
       if (nickname) {
-        const user = msg.from.replace('@c.us', ''); // Extracting the user's phone number
+        const user = Userid; // Extracting the user's phone number
         updateNickname(user, nickname);
         msg.reply(`Nickname set! Your nickname is now "${nickname}".`);
       } else {
         msg.reply('Please provide a nickname after the command. For example, `!setnickname MyNickname`.');
       }
     }
+	else if (msg.body.startsWith('!hallo')) {
+		msg.reply(`Hallo, ik ben Esculaap! Mijn belangrijkste functie is om alle punten bij te houden. Stuur mij een privé bericht met de tekst !help voor meer informatie.`);
+	}
     else if (msg.body.startsWith('!nieuw')) {
       const nickname = msg.body.split(' ')[1]; // Extract the nickname from the message
 
       if (nickname) {
-        const user = msg.from.replace('@c.us', ''); // Extracting the user's phone number
+        const user = Userid; // Extracting the user's phone number
         insertNewUser(user, nickname);
         msg.reply(`New user added! Your nickname is "${nickname}" and your score is initialized to 0.`);
       } else {
         msg.reply('Please provide a nickname after the command. For example, `!nieuw MyNickname`.');
       }
     }
-      else if (msg.body.includes('#punten')) {
-      const user = msg.from.replace('@c.us', ''); // Extracting the user's phone number
+      else if (msg.body.startsWith('#punten')) {
+      const user = Userid; // Extracting the user's phone number
       const currentScore = await getScore(user);
 
       // Check if the message contains a "+" or "-" followed by a number
-      const addScoreRegex = /([+-]\d+)/;
+      const addScoreRegex = /([+-]\d+)(?:\s(.+))?/;
       const addScoreMatch = msg.body.match(addScoreRegex);
 
       if (addScoreMatch) {
         const scoreChange = parseInt(addScoreMatch[1]);
+        const chick = addScoreMatch[2] || ''; // If no chick text is provided, use an empty string
+
         if (!isNaN(scoreChange)) {
           const newScore = currentScore + scoreChange;
           updateScore(user, newScore);
-          msg.reply(`Score updated! Your new score is ${newScore}.`);
+          logPoints(user, scoreChange, chick);
+          msg.reply(`Congratulations! Your new score is ${newScore}.`);
         }
       }
+	  if (msg.hasMedia) {
+        const media = await msg.downloadMedia();
+		const person = msg['_data']['notifyName'];
+		const time = new Date(msg.timestamp * 1000).toISOString().replace(/T/, ' ').replace(/\..+/, '').split(' ')[1].replaceAll(':', '-')
+		const date = new Date(msg.timestamp * 1000).toISOString().substring(0, 10);
+        const folder = process.cwd() + '/img/'+ '_' + person + '/' + date + '/';
+        const filename = folder + time + '_' + msg.id.id + '.' + media.mimetype.split('/')[1];
+        fs.mkdirSync(folder, { recursive: true });
+        fs.writeFileSync(filename, Buffer.from(media.data, 'base64').toString('binary'), 'binary');
+		}
     }
-  } else { // Mag in elke chat. Misschien nog maken, enkel in privé chat.
+  } else {
 	  if (msg.body.startsWith('!help')) {
-		  msg.reply(`Ik ben Esculaap, ik zorg er voor dat alle punten worden bijgehouden..`);
-		  const media = MessageMedia.fromFilePath('./snake.gif');
-		  msg.reply(media);
+		  client.sendMessage(msg.from,'Ik ben Esculaap, ik zorg er voor dat alle punten worden bijgehouden..');
+//		  const media = MessageMedia.fromFilePath('./snake.gif');
+//		  client.sendMessage(msg.from,media,(sendVideoAsGif=true));
+		  client.sendMessage(msg.from,'Er zijn een aantal opdrachten die ik begrijp: \n !setnickname MyNickname');	
+	  }
+	  else if (msg.body.startsWith('!log')){
+		  const user = msg.from; // Extracting the user's phone number
+		  console.log(`Getting log for user ${user}`);
+		  const userPointsLog = await getPointsLogForUser(user);
+		  if (userPointsLog.length > 0) {
+			const logMessage = userPointsLog
+				.map(({ score_change, chick, timestamp }) => `Datum: ${timestamp}, Punten: ${score_change}, Chick: ${chick}`)
+				.join('\n');
+				msg.reply(`Points Log:\n${logMessage}`);
+			} else {
+			msg.reply('No points log found.');
+			}
 	  }
     
   }
@@ -192,12 +227,28 @@ process.on('exit', () => {
   });
 });
 
-function logPointsChange(user, pointsChange) {
-  db.run('INSERT INTO points_changes (user, points_change) VALUES (?, ?)', [user, pointsChange], (err) => {
-    if (err) {
-      console.error('Error logging points change:', err.message);
-    } else {
-      console.log(`Points change logged for user ${user}. Points Change: ${pointsChange}`);
+function logPoints(user, scoreChange, chick) {
+  db.run(
+    'INSERT INTO points_log (user, score_change, chick) VALUES (?, ?, ?)',
+    [user, scoreChange, chick],
+    (err) => {
+      if (err) {
+        console.error('Error logging points:', err.message);
+      } else {
+        console.log(`Points logged for user ${user}. Score change: ${scoreChange}, Chick: ${chick}`);
+      }
     }
+  );
+}
+
+function getPointsLogForUser(user) {
+  return new Promise((resolve, reject) => {
+    db.all('SELECT score_change, chick, timestamp FROM points_log WHERE user = ? ORDER BY timestamp', [user], (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
   });
 }
